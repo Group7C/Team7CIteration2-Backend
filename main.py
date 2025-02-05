e08ac0ba0ee13f18c5524b5c8581fa93b2f9d8b1
JibPython <jibrail.ali.2005@gmail.com>
2025-02-05 15:50:13 +0000

    created database connection

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 from flask import Flask
+import psycopg2
 
 app = Flask(__name__)
+
+# this gets the information so you are able to connect to the database
+def get_database_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="software_engineering_iteration2",
+        user="admin",
+        password="1234"
+    )
+
+    return conn
+
+# this uses connects to the database with the information provided
+def open_database_connection(connection):
+    connection.cursor()
+
+# this closes the currently running database connection
+def close_database_connection(current_connection, database):
+    # closes the current connection first
+    current_connection.close()
+    database.close()
 
 
 # the url in the .route will be used to trigger the function
 @app.route("/")
-def hello_world():
+def index():
     return "<h1>working<h1>"
 
+@app.route("/check/user/exists")
+def checkUserExists():
+    return "<h1>Checking if user exists...<h1>"
 
+@app.route("/create/profile")
+def createProfile():
+    return "<h1>creating user profile...<h1>"
 
 # THE COMMAND BELOW IS HOW TO RUN THE FILE
 # flask --app main run
